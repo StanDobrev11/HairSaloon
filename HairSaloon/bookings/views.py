e65021919a56f895003d0a3803f5bda478708a02
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-03-20 00:27:08 +0200

    fixed bookings proper display and redirect to same page only msg to be displayed

--- HairSaloon/bookings/views.py
+++ HairSaloon/bookings/views.py
@@ -1,7 +1,8 @@
-from datetime import timedelta
+from datetime import timedelta, datetime
 from django.contrib import messages
 from django.core.serializers import serialize
 from django.http import HttpResponse, JsonResponse
+from django.urls import reverse_lazy
 from django.utils import timezone
 from django.views import generic as views
 
@@ -34,32 +35,38 @@ def bookings_json(request):
 class BookingView(views.FormView):
     template_name = 'bookings/dashboard.html'
     form_class = BookingForm
-    success_url = HttpResponse('Success')
+    # success_url = reverse_lazy('dashboard')
 
     def get_context_data(self, **kwargs):
         context = super(BookingView, self).get_context_data(**kwargs)
         bookings = Booking.objects.all().prefetch_related()
         context['bookings'] = bookings
         context['profile'] = Profile.objects.get(user=self.request.user)
-        context['upcoming_bookings'] = bookings.filter(date__lte=timezone.now())
-        context['passed_bookings'] = bookings.filter(date__gte=timezone.now())
+        context['upcoming_bookings'] = bookings.filter(date__gte=datetime.today())
+        context['passed_bookings'] = bookings.filter(date__lte=datetime.today())
 
         return context
+
+    def get_end_time(self, booking):
+
+        dummy_date = datetime(1, 1, 1)  # Year, month, day
+        start_datetime = datetime.combine(dummy_date, booking.start)
+        end_datetime = start_datetime + timedelta(minutes=booking.service.duration)
+
+        return end_datetime.time()
 
     def form_valid(self, form):
         booking = form.save(commit=False)
         booking.user = self.request.user
+        booking.end = self.get_end_time(booking)
 
         available_hairdresser = Booking.get_hairdresser(booking)
 
         if available_hairdresser:
             booking.hairdresser = available_hairdresser[0]
-            # calculating and saving end time
-            booking.end = (booking.start + timedelta(minutes=booking.service.duration)).time()
-
             booking.save()
             messages.success(self.request, 'Your booking has been successfully created.')
-            return super().form_valid(form)
+            return self.render_to_response(self.get_context_data(form=self.form_class()))
         else:
             # Instead of using form_invalid, manually add an error to the form
             # and re-render the template with the form containing errors.
