41271510bc78d877d7f4f2d90b97325677a56636
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-04-15 22:53:23 +0300

    Initial commit

--- HairSaloon/settings.py
+++ HairSaloon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#l0zcp&djw2cp9*y@1u)rt)7sum#!aa!j)i*w_uati#aafwyw='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', ]
 
