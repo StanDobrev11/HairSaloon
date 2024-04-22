e5590c00d0ceddc4f127685a0d772d573131218d
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-04-22 22:36:41 +0300

    updated service method for saving images

--- HairSaloon/services/models.py
+++ HairSaloon/services/models.py
@@ -1,8 +1,9 @@
 from io import BytesIO
 
 import requests
-from django.core.files import File
+from django.core.files.base import ContentFile
 from django.db import models
+from django.utils.crypto import get_random_string
 
 
 # Create your models here.
@@ -28,16 +29,37 @@ class Service(models.Model):
         return self.name
 
     def download_and_save_image(self):
-        # in order to download from url first time then save to local storage
-        # this function and the imports must be present
-        # then the save() method should be overwritten to call this method
-
+        """
+        Download image from `haircut_url` and save it to `haircut_photo`.
+        """
         if self.haircut_url and not self.haircut_photo:
-            response = requests.get(self.haircut_url)
-            if response.status_code == 200:
-                fp = BytesIO(response.content)
-                file_name = self.haircut_url.split('/')[-1]  # Assumes the URL has a filename at the end
-                self.haircut_photo.save(file_name, File(fp), save=False)
+            try:
+                response = requests.get(self.haircut_url, timeout=10)  # Adding timeout for the request
+                response.raise_for_status()  # Will raise an exception for HTTP error codes
+
+                # Content of the response is the image file
+                image_content = response.content
+                file_name = self.haircut_url.split('/')[-1]
+
+                # Remove URL parameters if any (after a '?')
+                file_name = file_name.split('?')[0]
+
+                # Ensuring the file name ends with a valid image extension
+                if not any(file_name.lower().endswith(ext) for ext in ['.jpg', '.jpeg', '.png', '.gif']):
+                    file_name += '.jpg'  # Defaulting to .jpg if no valid extension is found
+
+                # Creating a Django `File` object with a unique name
+                # Using `get_random_string` to avoid file name collisions
+                unique_file_name = f"{get_random_string(length=32)}_{file_name}"
+
+                self.haircut_photo.save(unique_file_name, ContentFile(image_content), save=False)
+
+            except requests.RequestException as e:
+                # Handle request exceptions (like network issues)
+                print(f"Failed to download image due to: {e}")
+            except Exception as e:
+                # Handle other potential exceptions
+                print(f"An error occurred: {e}")
 
     def save(self, *args, **kwargs):
         self.download_and_save_image()
