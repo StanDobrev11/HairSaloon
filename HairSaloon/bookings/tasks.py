89a46f5f6e912411689a2c52cd7cc9b11d5e91a2
MASTER-PC <dobrev81@gmail.com>
2025-04-18 13:23:06 +0000

    updated email service

--- HairSaloon/bookings/tasks.py
+++ HairSaloon/bookings/tasks.py
@@ -1,14 +1,16 @@
 from celery import shared_task
 from django.core.mail import send_mail
 
+from HairSaloon import settings
+
 
 @shared_task
 def send_async_email_new_booking(user_email, hairdresser_email, created_at):
     send_mail(
         subject=f'Booking created at {created_at}',
         message='Booking created successfully',
-        from_email='hair@saloon.com',
-        recipient_list=[user_email, hairdresser_email],
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user_email],
         fail_silently=False,
     )
 
@@ -18,7 +20,7 @@ def send_async_email_cancelled_booking(user_email, hairdresser_email, created_at
     send_mail(
         subject=f'Booking {created_at} cancelled',
         message='Booking cancelled successfully',
-        from_email='hair@saloon.com',
-        recipient_list=[user_email, hairdresser_email],
+        from_email=settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[user_email],
         fail_silently=False,
     )
